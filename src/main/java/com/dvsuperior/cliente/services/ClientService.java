0d01e3f6108d82773d4231b0db41306a44fd4f45
André Fernandes Dias <andredias34@hotmail.com>
2024-05-13 13:28:49 -0300

    novo client usando post

--- src/main/java/com/dvsuperior/cliente/services/ClientService.java
+++ src/main/java/com/dvsuperior/cliente/services/ClientService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -27,6 +28,20 @@ public class ClientService {
     public Page<ClientDTO> findAll(Pageable pageable){
         Page<Client> result = repository.findAll(pageable);
         return result.map(x -> new ClientDTO(x));
+    }
+
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client entity = new Client();
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
     }
 }
 
